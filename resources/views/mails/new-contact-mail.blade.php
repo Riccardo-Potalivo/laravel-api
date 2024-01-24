9ffab4b5f6631eb1a4fc316b793b374399452ff9
Riccardo Potalivo <riccardopotalivo@gmail.com>
2024-01-24 15:32:27 +0100

    new contact & view set

---
+++ resources/views/mails/new-contact-mail.blade.php
@@ -0,0 +1,11 @@
+<div>
+    <h1>Ciao Admin</h1>
+
+    <p>
+        hai ricevuto un seddaggio, ecco i dettagli: <br>
+        Nome: {{ $lead->name }} <br>
+        Email: {{ $lead->email }} <br>
+        Address: {{ $lead->address }} <br>
+        Message: {{ $lead->message }} <br>
+    </p>
+</div>
