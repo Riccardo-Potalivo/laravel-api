e9d2ffdb9c21bc5bb1a6dfc7e69c3f24eb449a50
Riccardo Potalivo <riccardopotalivo@gmail.com>
2024-01-24 16:27:07 +0100

    tiket

--- app/Http/Controllers/LeadController.php
+++ app/Http/Controllers/LeadController.php
@@ -10,21 +10,6 @@ use App\Mail\NewContact;
 
 class LeadController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
@@ -55,37 +40,5 @@ class LeadController extends Controller
         return response()->json([
             'success' => true,
         ]);
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(Lead $lead)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Lead $lead)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Lead $lead)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Lead $lead)
-    {
-        //
     }
 }
